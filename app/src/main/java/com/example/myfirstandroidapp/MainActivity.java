a9752d576ee7470eb2fdb8f3054f16c9e7cac541
Jordan <jsamson2@academic.rrc.ca>
2023-01-03 22:40:48 -0600

    Initial Commit

---
+++ app/src/main/java/com/example/myfirstandroidapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.myfirstandroidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "Greetings Class, looking forward to a great term!", Toast.LENGTH_SHORT).show();
+
+    }
+}
